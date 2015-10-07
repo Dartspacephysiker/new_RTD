7403c745dd8e44351e0456704bdbe69b364b8735
Spencer Hatch (Vær så godt) <hammerhatch@gmail.com>
2015-10-07 12:56:35 -0400

    Added two panels, anyway. Keep it moving.

--- dartRTD.cpp
+++ dartRTD.cpp
@@ -49,26 +49,50 @@ bool DartRTDApp::OnInit()
     
     DartRTDFrame *frame = new DartRTDFrame( "Dartmouth RTD", wxPoint(50, 50), wxSize(450, 340) );
     frame->Show( true );
+
     return true;
 }
+
 DartRTDFrame::DartRTDFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
         : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-    wxMenu *menuFile = new wxMenu;
+    /*File menu*/
+    menuFile = new wxMenu;
     menuFile->Append(ID_LoadRTDConf, "&Load RTD Config...\tCtrl-L",
                      "Load an RTD configuration file");
     menuFile->AppendSeparator();
     menuFile->Append(wxID_EXIT);
-    wxMenu *menuView = new wxMenu;
-    wxMenu *menuHelp = new wxMenu;
+
+    /*View menu*/
+    menuView = new wxMenu;
+    
+    /*Help menu*/
+    menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
-    wxMenuBar *menuBar = new wxMenuBar;
+
+    /*Menu bar*/
+    menuBar = new wxMenuBar;
     menuBar->Append( menuFile, "&File" );
     menuBar->Append( menuView, "&View" );
     menuBar->Append( menuHelp, "&Help" );
+
     SetMenuBar( menuBar );
     CreateStatusBar();
     SetStatusText( "Welcome to Dartmouth RTD!" );
+
+    m_parent = new wxPanel(this, wxID_ANY);
+
+    wxBoxSizer *hbox = new wxBoxSizer(wxHORIZONTAL);
+    
+    m_lp = new SpectrogramPanel(m_parent);
+    m_rp = new SpectrListPanel(m_parent);
+    
+    hbox->Add(m_lp, 1, wxEXPAND | wxALL, 5);
+    hbox->Add(m_rp, 1, wxEXPAND | wxALL, 5);
+    
+    m_parent->SetSizer(hbox);
+    
+    this->Centre();
 }
 void DartRTDFrame::OnExit(wxCommandEvent& event)
 {
@@ -82,4 +106,44 @@ void DartRTDFrame::OnAbout(wxCommandEvent& event)
 void DartRTDFrame::OnLoadRTDConf(wxCommandEvent& event)
 {
     wxLogMessage("Bout to load dat");
+}
+
+
+SpectrogramPanel::SpectrogramPanel(wxPanel * parent)
+       : wxPanel(parent, -1, wxPoint(-1, -1), wxSize(-1, -1), wxBORDER_SUNKEN)
+{
+  count = 0;
+  m_parent = parent;
+  m_plus = new wxButton(this, ID_PLUS, wxT("+"), 
+      wxPoint(10, 10));
+  m_minus = new wxButton(this, ID_MINUS, wxT("-"), 
+      wxPoint(10, 60));
+  Connect(ID_PLUS, wxEVT_COMMAND_BUTTON_CLICKED, 
+      wxCommandEventHandler(SpectrogramPanel::OnPlus));
+  Connect(ID_MINUS, wxEVT_COMMAND_BUTTON_CLICKED, 
+      wxCommandEventHandler(SpectrogramPanel::OnMinus));
+}
+
+void SpectrogramPanel::OnPlus(wxCommandEvent & WXUNUSED(event))
+{
+  count++;
+
+  DartRTDFrame *comm = (DartRTDFrame *) m_parent->GetParent();
+  comm->m_rp->m_text->SetLabel(wxString::Format(wxT("%d"), count));
+}
+
+void SpectrogramPanel::OnMinus(wxCommandEvent & WXUNUSED(event))
+{
+  count--;
+
+  DartRTDFrame *comm = (DartRTDFrame *) m_parent->GetParent();
+  comm->m_rp->m_text->SetLabel(wxString::Format(wxT("%d"), count));
+}
+
+
+SpectrListPanel::SpectrListPanel(wxPanel * parent)
+       : wxPanel(parent, wxID_ANY, wxDefaultPosition, 
+         wxSize(270, 150), wxBORDER_SUNKEN)
+{
+    m_text = new wxStaticText(this, -1, wxT("0"), wxPoint(40, 60));
 }
--- dartRTD.h
+++ dartRTD.h
@@ -6,6 +6,9 @@
 
 #include <fstream>
 
+const int ID_PLUS = 101;
+const int ID_MINUS = 102;
+
 class DARTRTD_Measurement { 
 
   private:
@@ -31,10 +34,48 @@ public:
     virtual bool OnInit();
 };
 
-class DartRTDFrame: public wxFrame
+/*The panel that holds the spectrograms*/
+class SpectrogramPanel : public wxPanel
 {
 public:
+    SpectrogramPanel(wxPanel *parent);
+
+    void OnPlus(wxCommandEvent & event);
+    void OnMinus(wxCommandEvent & event);
+
+    wxButton *m_plus;
+    wxButton *m_minus;
+    wxPanel *m_parent;
+    int count;
+
+};
+
+/*The panel that holds the list of available spectrograms*/
+class SpectrListPanel : public wxPanel
+{
+public:
+    SpectrListPanel(wxPanel *parent);
+
+    void OnSetText(wxCommandEvent & event);
+
+    wxStaticText *m_text;
+
+};
+
+class DartRTDFrame: public wxFrame
+{
+    wxMenu             * menuFile;
+    wxMenu             * menuView;
+    wxMenu             * menuHelp;
+		       
+    wxMenuBar          * menuBar;
+
+public:
     DartRTDFrame(const wxString& title, const wxPoint& pos, const wxSize& size);
+    SpectrogramPanel   * m_lp;
+    SpectrListPanel    * m_rp;
+    wxPanel            * m_parent;
+
 private:
     void OnHello(wxCommandEvent& event);
     void OnLoadRTDConf(wxCommandEvent& event);
